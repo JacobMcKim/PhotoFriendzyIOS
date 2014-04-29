d38157c3b2ed1653a372f550a3e0cb5c18d8c23e
JacobMcKim <mckim.jacob@gmail.com>
2014-04-29 02:52:27 -0400

    Initial Setup

---
+++ PhotoFriendzy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoFriendzy
+//
+//  Created by McKim™ on 4/29/14.
+//  Copyright (c) 2014 Code Storm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
